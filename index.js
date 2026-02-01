9c2dbaa06e7296d62dead36b3328b77111628c1c
Gaspar <gaspar.deutsch@gmail.com>
2026-02-01 20:08:02 +0100

    :gear: edited index.js

--- index.js
+++ index.js
@@ -8,16 +8,14 @@ const searchEmpty = document.querySelector(".search-empty")
 const tagsContainer = document.querySelector(".tags");
 
 let allMovies = []
-let filmCategorie = []
-
 let selectedCategories = []
 let searchValue = ""
 //---------------------
 
 
-function createListItems(data) {
+function createListItems(movies) {
     let html = ""
-    data.forEach(movie => { // pour chaque film ça génère une card
+    movies.forEach(movie => { // pour chaque film ça génère une card
         html += `
                     <a class="card" href="scripts/movie.html?id=${movie.id}">
                         <img class="poster" src="${movie.img}" alt="Affiche ${movie.name}">
@@ -28,40 +26,35 @@ function createListItems(data) {
                 `
     });
     gridContainer.innerHTML = html // innerHTML de toute les cards dans gridContainer
-}
 
+    console.log(movies.length);
 
-axios.get(`${apiURL}/movies`)
-    .then(response => {
-
-        allMovies = response.data.data // récupère tout les film et les stocke dans allMovies
-
-        createListItems(allMovies) // appel la fonction createListItems avec tout les films
-
-    })
-
-// BARRE DE RECHERCHE
-searchInput.addEventListener("input", (e) => {
-    searchEmpty.style.display = "none"; // cache le message "pas de résultat" à chaque input
-
-    const currentValue = e.target.value // récupère la valeur actuel dans l'input et le stock dans currentValue
-    gridContainer.innerHTML = "" // à chaque inpput remet le contenue vide
-    const dataFilter = allMovies.filter(movie => movie.name.toLowerCase().includes(currentValue.toLowerCase()) ||
-        movie.author.toLowerCase().includes(currentValue.toLowerCase()))
-    //  || movie.description.toLowerCase().includes(currentValue.toLowerCase())) // filtre la liste de tout les films, compare le nom des films en minuscule avec la valeur dans l'input en minuscule et les stocke dans dataFilter
-
-    if (dataFilter.length === 0) { // si il n'y a pas de film dans dataFilter alors afficher le message "pas de résultat"
-
-        searchEmpty.style.display = "flex";
+    if (movies.length === 0) { // vérifie si aucun film apparait
+        searchEmpty.style.display = "flex"; // si vrai, affiche le message "pas de résultat"
+    } else {
+        searchEmpty.style.display = "none"; // sinon le cache
     }
-
-    createListItems(dataFilter)
-})
-// il faut que je mettes le système de barre de recherche dans la même fonction que matchCategorie pour qu'a chaque input je puisse appeler les fonctions matchSearch et matchCategories
+}
 
 
 function filterMovies() {
     const filter = allMovies.filter(movie => {
+
+        // gridContainer.innerHTML = "" // à chaque inpput remet le contenue vide
+        console.log("----------------------------------------");
+        
+        console.log("searchValue = ", searchValue);
+        console.log("Movie name : ", movie.name);
+        
+        console.log("Movie name includes searchValue : ", movie.name.toLowerCase().includes(searchValue.toLowerCase()));
+        console.log("Movie author includes searchValue : ", movie.author.toLowerCase().includes(searchValue.toLowerCase()));
+        
+        
+        
+
+        const matchSearch = movie.name.toLowerCase().includes(searchValue.toLowerCase()) || movie.author.toLowerCase().includes(searchValue.toLowerCase())
+        //  || movie.description.toLowerCase().includes(searchValue.toLowerCase())) // filtre la liste de tout les films, compare le nom des films en minuscule avec la valeur dans l'input en minuscule et les stocke dans matchSearch
+
         let matchCategories
 
         if (selectedCategories === 0) {
@@ -70,10 +63,29 @@ function filterMovies() {
             matchCategories = movie.categories.some(caté => selectedCategories.includes(caté.id))
         }
 
-        
+        console.log("matchCategories : ", matchCategories);
+        console.log("matchSearch : ", matchSearch);
+        return matchSearch && matchCategories
     })
+
     console.log(filter);
+    
+    createListItems(filter)
 }
+
+// BARRE DE RECHERCHE
+searchInput.addEventListener("input", (e) => {
+    searchValue = e.target.value // récupère la valeur actuel dans l'input et le stock dans searchValue
+    filterMovies()
+})
+
+// RÉCUPÈRE LES FILMS
+axios.get(`${apiURL}/movies`)
+    .then(response => {
+        allMovies = response.data.data // récupère tout les film et les stocke dans allMovies
+
+        createListItems(allMovies) // appel la fonction createListItems avec tout les films
+    })
 
 
 // GÉNÈRE LES CATÉGORIES
@@ -81,52 +93,23 @@ axios.get(`${apiURL}/categories`)
     .then(response => {
         response.data.data.forEach((categorie) => {
 
-            const createTag = document.createElement("div") // créer une div
-            createTag.classList.add("tag") // ajoute la class tag
-            createTag.dataset.id = categorie.id // ajout dataset avec son id
-            createTag.textContent = categorie.name
+            const tag = document.createElement("div") // créer une div
+            tag.classList.add("tag") // ajoute la class tag
+            tag.dataset.id = categorie.id // ajout dataset avec son id
+            tag.textContent = categorie.name
 
-            document.querySelector(".tags").appendChild(createTag); // ajoute les tags créer dans la div tags
 
-            createTag.addEventListener("click", (e) => {
-                e.currentTarget.classList.toggle("selected") // toggle la class selected
+            tag.addEventListener("click", () => {
+                tag.classList.toggle("selected") // toggle la class selected
 
-                filmCategorie = []; // réinitialise le tableau à chaque tag selected
+                selectedCategories = []; // réinitialise le tableau à chaque tag selected
 
-                document.querySelectorAll(".selected").forEach(element => {
-                    filmCategorie.push(parseInt(element.dataset.id)) // converti l'id en nombre et l'ajoute dans le tableau avec push
+                document.querySelectorAll(".tag.selected").forEach(element => {
+                    selectedCategories.push(parseInt(element.dataset.id)) // converti l'id en nombre et l'ajoute dans le tableau avec push
                 })
+
+                filterMovies()
             });
+            document.querySelector(".tags").appendChild(tag); // ajoute les tags créer dans la div tags
         })
     })
-
-
-
-
-
-// FILTRE
-
-// comparé l'id des catégorie du film avec celle "selected"
-
-
-// const sectionChars = document.querySelector('.personnages').closest('section');
-// const checkboxesChar = sectionChars.querySelectorAll('input[type="checkbox"]');
-// const characters = document.querySelectorAll('.personnages a');
-
-// checkboxesChar.forEach(checkbox => {
-//     checkbox.addEventListener('change', () => {
-//         const activeElements = [...checkboxesChar]
-//             .filter(cb => cb.checked)
-//             .map(cb => cb.id);
-
-//         if (activeElements.length === 0) {
-//             characters.forEach(char => char.style.display = 'flex');
-//             return;
-//         }
-
-//         characters.forEach(char => {
-//             const hasMatch = activeElements.some(el => char.classList.contains(el));
-//             char.style.display = hasMatch ? 'flex' : 'none';
-//         });
-//     });
-// });
